48acbf4f49c64231be30f6c2f4717a1ed8889e3e
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-08-17 09:57:20 +0200

    Added migration for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_082744) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_17_074856) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "sessions", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "token", null: false
     t.bigint "user_id", null: false
@@ -32,5 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_082744) do
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "sessions", "users"
 end
