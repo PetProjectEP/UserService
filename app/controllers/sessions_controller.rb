d9720a94af4828f23693f13c3fd3656a556cd975
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-11-08 09:54:39 +0200

    Rewrote sessions controller to early return pattern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,31 +3,31 @@ class SessionsController < ApplicationController
 
   # Returns user by auth_token in get param
   def show
-    if @session
-      @user = Rails.cache.fetch(@session[:user_id]) do
-        User.find(@session[:user_id]).slice(:id, :name, :surname) # No need for storing any sensetive info
-      end
-
-      render json: @user.to_json(only: %i[id name surname])
-    else
+    unless @session
       render json: { error: "Token is invalid" }, status: :unprocessable_entity
     end
+
+    @user = Rails.cache.fetch(@session[:user_id]) do
+      User.find(@session[:user_id]).slice(:id, :name, :surname) # No need for storing any sensetive info
+    end
+
+    render json: @user.to_json(only: %i[id name surname])
   end
 
   def create
     @user = User.find_by(nickname: auth_params[:nickname])
 
-    if @user && @user.authenticate(auth_params[:password])
-      @session = Session.new(user_id: @user.id)
-
-      if @session.save
-        render json: @session, status: :created, location: @session
-      else
-        render json: @session.errors, status: :unprocessable_entity
-      end
-    else
+    unless @user && @user.authenticate(auth_params[:password])
       render json: { error: "Authentication data are invalid" }, status: :unprocessable_entity
-    end 
+    end
+
+    @session = Session.new(user_id: @user.id)
+
+    if @session.save
+      render json: @session, status: :created, location: @session
+    else
+      render json: @session.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
